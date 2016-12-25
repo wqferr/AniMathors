af3ca76ebe134b578a00b6cc7bf80c808cf4ab4c
wqferr <wqferr@gmail.com>
2016-12-25 14:44:10 -0200

    Add more softening functions

--- core/softening.py
+++ core/softening.py
@@ -29,3 +29,28 @@ def sinexp(a):
         t = _clamp(0, 1, t)
         return 1 - np.sin(np.pi/2 * np.exp(-a*t*t) * (1-t)*(1-t))
     return f
+
+def bezier3(p):
+    """ Creates the Beziér curve (0, p, 1) """
+    def f(t):
+        return 2*(1-t)*t*p + t*t
+    return f
+
+def bezier4(p1, p2):
+    """ Creates the Beziér curve (0, p1, p2, 1) """
+    def f(t):
+        t = _clamp(0, 1, t)
+        tc2 = (1-t)*(1-t)
+        return 3*tc2*t*p1 + 3*(1-t)*t*t*p2 + t*t*t
+
+    return f
+
+def ease_in(f):
+    return bezier3(1/10**f)
+
+def ease_out(f):
+    return bezier3(1 - 1/10**f)
+
+def ease_in_out(f):
+    f = 1/10**f
+    return bezier4(f, 1-f)
